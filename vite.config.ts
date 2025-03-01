8ba5a4d603ae7582d231f2c51d0560f1a43f0c8a
Jeevan <103121525+j-e-e-v-a-n@users.noreply.github.com>
2025-03-01 10:07:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  server: {
+    port: 5173, // Optional: Change the local dev port
   },
 });
